4e8b4edc9407feee34ba03ceb0d9d2f3d05b0eba
root <root@Timofey>
2026-03-26 01:19:21 +0700

    Произвел изминения в тестах, test.yml, vite.config

--- src/tests/task8.test.ts
+++ src/tests/task8.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { csvToJSON } from '../tasks/task8';
+import { csvToJSON } from '../tasks/task8.js';
 
 describe('Задание 8: csvToJSON', () => {
     describe('Корректные входные данные', () => {
