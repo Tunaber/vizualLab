a034fb211c6a39b817f296659464872f7694fbef
root <root@Timofey>
2026-03-26 01:11:13 +0700

    Поправил tsconfig and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
-export default defineConfig ({
-    test: {
-        environment: 'node',
-        include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
-        globals: false,
-        coverage: {
-            provider: 'v8',
-            reporter: ['text', 'lcov', 'html'],
-            reportsDirectory: 'coverage'
-        }
-    }
-})
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.test.ts',
+        '**/*.spec.ts',
+      ],
+    },
+  },
+});
